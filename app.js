6f22d80a28497935c8f7550f8adcdf0a1fbcfb84
huseynovnabi huseynovnabi <huseynovnabi2002@gmail.com>
2024-01-17 11:42:59 +0000

    appChange

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 "use strict";
 $(document).ready(function () {
-    $(".btn").click(function (e) {
+
+    $('.btn').click(function (e) {
         e.preventDefault();
-        if (!$.trim($("input").val()) == "") {
+        if (!$.trim($('input').val()) == '') {
             let a = b => {
-                for (let i = 0; i < b; i++) { $(".minibox").append(`<h2>${b}</h2>`); }
+                for (let i = 0; i < b; i++) { $('.minibox').append(`<h2>${b}</h2>`); }
             }
-            let v = f => Number(f) ? a(f) : $(".minibox").append(`<h2>${f}</h2>`);
-            v($("input").val());
+            let v = f => Number(f) ? a(f) : $('.minibox').append(`<h2>${f}</h2>`);
+            v($('input').val());
         }
-        $("input").val("");
+        $('input').val('');
     })
+
 });
